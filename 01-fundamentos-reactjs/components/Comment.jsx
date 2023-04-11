ecc91667f11b1fe0adbf3b6d98d01aa3901da4a9
Felipe Oliveira <lipe111094@hotmail.com>
2023-04-11 15:48:24 -0300

    Função Aplaudir

--- 01-fundamentos-reactjs/components/Comment.jsx
+++ 01-fundamentos-reactjs/components/Comment.jsx
@@ -2,10 +2,17 @@ import { Trash } from "phosphor-react";
 import { ThumbsUp } from "phosphor-react";
 import { Avatar } from "./Avatar";
 import styles from "./Comment.module.css";
+import { useState } from "react";
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
   }
   return (
     <div className={styles.comment}>
@@ -27,9 +34,9 @@ export function Comment({ content, onDeleteComment }) {
           <p>{content}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
